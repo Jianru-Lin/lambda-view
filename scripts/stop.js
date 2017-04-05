e299ff1b8f7787775df341619bc9bb4d400497b2
Jianru Lin <jianru.lin@gmail.com>
2017-04-05 14:29:08 +0800

    catch script error

--- scripts/stop.js
+++ scripts/stop.js
@@ -1,8 +1,13 @@
-console.log('stopping lambda-view web server...')
-require('fmtjs-web').stop(function(err) {
-	if (err) {
-		console.error(err.message)
-		return
-	}
-	console.log('done.')
-})
+try {
+	console.log('stopping lambda-view web server...')
+	require('fmtjs-web').stop(function(err) {
+		if (err) {
+			console.error(err.message)
+			return
+		}
+		console.log('done.')
+	})	
+}
+catch (err) {
+	// console.error(err.message)
+}
